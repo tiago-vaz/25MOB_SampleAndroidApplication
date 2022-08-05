d8c4942f7d9b7a44d29d01206923e32e7e545ed8
Tiago Vaz <tiago-vaz@hotmail.com>
2022-08-05 09:02:46 -0300

    Atualizações da aula de 04/08/22

---
+++ app/src/main/java/com/example/a25mob_sampleandroidapplication/Onboarding.kt
@@ -0,0 +1,11 @@
+package com.example.a25mob_sampleandroidapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Onboarding : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
---
+++ app/src/main/java/com/example/a25mob_sampleandroidapplication/UserInfo.kt
@@ -0,0 +1,11 @@
+package com.example.a25mob_sampleandroidapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserInfo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_info)
+    }
+}
